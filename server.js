2ddb9c5b5e26a719cdb9f84d34b15bbc3aa833cb
Tcress1993 <cresswellt54@gmail.com>
2025-02-07 10:39:14 -0700

    server changes

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.post('/' , (req, res) => {
     res.json(responseBody);
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 const server = app.listen(port, () => {
     console.log(`Server running on port ${port}`);
